d94de6e80066efe183f78f1e97decebe652823b0
Rafael Pazos <rarod@microsoft.com>
2023-11-17 15:02:06 +0100

    small change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
 function App() {
   const value = 'World';
-  return <div>Hola {value}</div>;
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <h2>Hola {value}</h2>
+      <MyButton />
+    </div>
+  );
 }
 
 export default App;
