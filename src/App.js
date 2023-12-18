6af432a1cd2f2ce8e82b81b1197cd9b95a8742b0
Rafael Pazos <rafael.pazos@microsoft.com>
2023-12-18 16:38:10 +0100

    css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import About from './About';
 
 function MyButton() {
   function handleClick() {
@@ -11,28 +12,7 @@ function MyButton() {
   );
 }
 
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
 
-function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-    </>
-  );
-}
 
 
 function App() {
