b0b758f0315443522a666b16ee4396e3edd5ce41
Rafael Pazos <rafael.pazos@microsoft.com>
2023-12-19 16:15:23 +0100

    fix add grid

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GridLayout from "react-grid-layout";
 import About from './About';
 
 function MyButton() {
@@ -12,7 +13,11 @@ function MyButton() {
   );
 }
 
-
+const layout = [
+  { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
+  { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+  { i: "c", x: 4, y: 0, w: 1, h: 2 }
+];
 
 
 function App() {
@@ -23,6 +28,17 @@ function App() {
       <h2>Hola {value}</h2>
       <MyButton />
       <About />
+      <GridLayout
+        className="layout"
+        layout={layout}
+        cols={12}
+        rowHeight={30}
+        width={1200}
+      >
+        <div key="a">a</div>
+        <div key="b">b</div>
+        <div key="c">c</div>
+      </GridLayout>
     </div>
   );
 }
